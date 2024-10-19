82906028467f87ba7a43022c91451c3398707076
Brain Ambani <brainambani1@gmail.com>
2024-10-19 21:59:22 +0300

    add an incomplete trends side bar

---
+++ src/components/TrendsSidebar.tsx
@@ -0,0 +1,7 @@
+export default function TrendsSidebar() {
+  return (
+    <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
+      Trends sidebar
+    </div>
+  );
+}
